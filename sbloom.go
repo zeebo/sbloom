ad3f02c38f1f59b1ae83cff3c42261e669a02571
jeff <leterip@gmail.com>
2012-08-13 06:25:19 -0400

    some implementation work

--- sbloom.go
+++ sbloom.go
@@ -1,6 +1,9 @@
 package sbloom
 
-import "hash"
+import (
+	"hash"
+	"math/rand"
+)
 
 const (
 	elemSize = 3
@@ -23,44 +26,80 @@ func get(m []uint8, n uint64) bool {
 type Filter struct {
 	bh hash.Hash64
 	fs []*filter
+	hs []sHash
 }
 
-func (f Filter) Add(p []byte) {
+func (f *Filter) Add(p []byte) {
 	last := f.fs[len(f.fs)-1]
-	last.Add(p)
+	last.Add(p, f.hs[:last.k])
 	if last.left == 0 {
-		//this one filled, add a new one
+		newSize := last.size << 1
+		newMask := last.mask<<1 + 1
+		newK := last.k + 1
+		f.addNewFilter(newSize, newMask, newK)
 	}
 }
 
-func (f Filter) Lookup(p []byte) bool {
+func (f *Filter) Lookup(p []byte) bool {
 	for _, subfil := range f.fs {
-		if subfil.Lookup(p) {
+		if subfil.Lookup(p, f.hs[:subfil.k]) {
 			return true
 		}
 	}
 	return false
 }
 
+func (f *Filter) addNewFilter(size, mask uint64, k int) {
+	bins := make([][]uint8, k+1) //allocate one more bin
+	binSize := size >> elemSize  //size of each bin to have size bits
+	for i := range bins {
+		bins[i] = make([]uint8, binSize)
+	}
+
+	//make sure we have up to k hashes
+	for len(f.hs) < k {
+		f.hs = append(f.hs, sHash{
+			ha:   f.bh,
+			seed: randSeed(),
+		})
+	}
+
+	//add the new bloom filter
+	f.fs = append(f.fs, &filter{
+		size: size,
+		mask: mask,
+		bins: bins,
+		k:    k,
+		left: size / 5 * 7,
+	})
+}
+
+func randSeed() (p []byte) {
+	for i := 0; i < 4; i++ {
+		p = append(p, byte(rand.Intn(256)))
+	}
+	return
+}
+
 type filter struct {
-	size uint   // N == 1 << size
+	size uint64 // N == 1 << size
 	mask uint64 // x % N == x & mask == x & (1 << size) - 1
 
+	k    int
 	bins [][]uint8 // [k][1<<size]uint8 bins
-	hs   []sHash   // [k]sHash hashes
 	left uint64    // number of additions left until new filter
 }
 
-func (f *filter) Add(p []byte) {
-	for i, h := range f.hs {
+func (f *filter) Add(p []byte, hs []sHash) {
+	for i, h := range hs {
 		val := h.Hash(p)
 		set(f.bins[i], val&f.mask)
 	}
 	f.left--
 }
 
-func (f *filter) Lookup(p []byte) bool {
-	for i, h := range f.hs {
+func (f *filter) Lookup(p []byte, hs []sHash) bool {
+	for i, h := range hs {
 		val := h.Hash(p)
 		if !get(f.bins[i], val&f.mask) {
 			return false
